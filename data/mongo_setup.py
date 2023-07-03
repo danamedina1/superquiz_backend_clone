6c3d4a5935fa1b12cf31ab6ed7ccde6c85d46b15
Dana <danamedina@gmail.com>
2023-07-03 10:38:57 +0300

    changes in .gitignore: added directories to ignore: __pycache__/ .idea data/__pycache__/

---
+++ data/mongo_setup.py
@@ -0,0 +1,9 @@
+import mongoengine
+
+#initialize the database
+def global_init(db_name: str):
+    mongoengine.register_connection(alias='core', name=db_name)
+
+#connect to the database
+def connect_db():
+    mongoengine.connect(db='mongoengine_superquizDB', alias='core')
